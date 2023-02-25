f4085f8de59273a5e5a36004eb212a44ee26d4d4
Denis Davydov <120593753+Denis-Davydov-get@users.noreply.github.com>
2023-02-25 08:48:58 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,11 +1,11 @@
-﻿string[] array1 = new string[5] { "123", "23", "hello", "world", "res" }; //объявление списка
+string[] array1 = new string[5] { "123", "23", "hello", "world", "res" }; //объявление списка
 string[] array2 = new string[array1.Length]; // создание списка для заполнения минимальными значениями списка array1
 void SearchMinElement(string[] array1, string[] array2) // метод который заполняет array2 из array1
 {
     int count = 0; //  дополнительный счетчик для перебора позиции массива
     for (int i = 0; i < array1.Length; i++)
     {
-        if (array1[i].Length <= 3) // если элемент меньше 3 символов, положить в список array2 наименьший элемент
+        if (array1[i].Length <= 3) // если элемент меньше или равно 3 символа, положить в список array2 наименьший элемент
         {
             array2[count] = array1[i]; 
             count++;
